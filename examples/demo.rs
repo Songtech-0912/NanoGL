a85335f8d808cb927ad6ea3bf5eccf0b8164cf74
Jacky Song <almey0721@gmail.com>
2022-08-14 23:09:56 -0400

    Working demo (but unsafe)

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,7 +5,17 @@ pub fn main() {
     let window: nanogl::GLWindow = nanogl::GLWindow::new(320, 240, "Window 1", 0);
     while window.is_running() {
         window.clear(nanogl::RGBColor(128, 144, 160));
-        window.print_bitmap(tfont, 120, 110, nanogl::RGBColor(0, 0, 0), "Hello world #1");
+        // Currently tfont is a mut static TigrFont, so it isn't thread-safe,
+        // this will be fixed eventually
+        unsafe {
+            window.print_bitmap(
+                tfont,
+                120,
+                110,
+                nanogl::RGBColor(255, 255, 255),
+                "Hello world!",
+            );
+        }
         window.update();
     }
     window.cleanup();
