932efe1e32788674c737a271fb62c4ab33cb4e25
Jacky Song <almey0721@gmail.com>
2022-08-14 22:31:43 -0400

    WIP for new safe API

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,57 +1,12 @@
-use nanogl::tigr::*;
+use nanogl;
+use nanogl::tigr::tfont;
 
-unsafe fn main_0(
-    mut _argc: libc::c_int,
-    mut _argv: *mut *mut libc::c_char,
-) -> libc::c_int {
-    let window: *mut Tigr = tigrWindow(
-        320 as libc::c_int,
-        240 as libc::c_int,
-        b"Window1\0" as *const u8 as *const libc::c_char,
-        0 as libc::c_int,
-    );
-    while tigrClosed(window) == 0 {
-        tigrClear(
-            window,
-            tigrRGB(
-                0x80 as libc::c_int as libc::c_uchar,
-                0x90 as libc::c_int as libc::c_uchar,
-                0xa0 as libc::c_int as libc::c_uchar,
-            ),
-        );
-        tigrPrint(
-            window,
-            tfont,
-            120 as libc::c_int,
-            110 as libc::c_int,
-            tigrRGB(
-                0xff as libc::c_int as libc::c_uchar,
-                0xff as libc::c_int as libc::c_uchar,
-                0xff as libc::c_int as libc::c_uchar,
-            ),
-            b"Hello, world #1.\0" as *const u8 as *const libc::c_char,
-        );
-        tigrUpdate(window);
-    }
-    tigrFree(window);
-    return 0 as libc::c_int;
-}
 pub fn main() {
-    let mut args: Vec::<*mut libc::c_char> = Vec::new();
-    for arg in ::std::env::args() {
-        args.push(
-            (::std::ffi::CString::new(arg))
-                .expect("Failed to convert argument into CString.")
-                .into_raw(),
-        );
+    let window: nanogl::GLWindow = nanogl::GLWindow::new(320, 240, "Window 1", 0);
+    while window.is_running() {
+        window.clear(nanogl::RGBColor(128, 144, 160));
+        window.print_bitmap(tfont, 120, 110, nanogl::RGBColor(0, 0, 0), "Hello world #1");
+        window.update();
     }
-    args.push(::core::ptr::null_mut());
-    unsafe {
-        ::std::process::exit(
-            main_0(
-                (args.len() - 1) as libc::c_int,
-                args.as_mut_ptr() as *mut *mut libc::c_char,
-            ) as i32,
-        )
-    }
+    window.cleanup();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -14,3 +14,81 @@
 pub mod tigr;
 use libc;
 
+pub struct RGBColor(pub i32, pub i32, pub i32);
+
+pub struct GLWindow {
+    width: i32,
+    height: i32,
+    title: String,
+    flag: i32,
+    window_ptr: *mut tigr::Tigr
+}
+
+impl GLWindow {
+    pub fn new(width: i32, height: i32, title: &str, flag: i32) -> GLWindow {
+        let window_ptr: *mut tigr::Tigr = unsafe {
+            tigr::tigrWindow(
+                width as libc::c_int,
+                height as libc::c_int,
+                title.as_bytes().as_ptr() as *const libc::c_char,
+                flag as libc::c_int,
+            )
+        };
+        GLWindow {
+            width: width,
+            height: height,
+            title: String::from(title),
+            flag: flag,
+            window_ptr: window_ptr
+        }
+    }
+
+    pub fn clear(&self, color: RGBColor) {
+        unsafe {
+            tigr::tigrClear(self.window_ptr, rgb(color.0, color.1, color.2));
+        }
+    }
+
+    pub fn update(&self) {
+        unsafe {
+            tigr::tigrUpdate(self.window_ptr);
+        }
+    }
+
+    pub fn is_running(&self) -> bool {
+        unsafe {
+            tigr::tigrClosed(self.window_ptr) as i32 == 0
+        }
+    }
+
+    pub fn print_bitmap(&self, font: *mut tigr::TigrFont, x: i32, y: i32, color: RGBColor, text: &str)
+    {
+        unsafe {
+            tigr::tigrPrint(
+                self.window_ptr,
+                font,
+                x as libc::c_int,
+                y as libc::c_int,
+                rgb(color.0, color.1, color.2),
+                text.as_bytes().as_ptr() as *const libc::c_char
+            );
+        }
+    }
+
+    pub fn cleanup(&self) {
+        unsafe {
+            tigr::tigrFree(self.window_ptr);
+        }
+    }
+}
+
+pub fn rgb(r: i32, g: i32, b: i32) -> tigr::TPixel {
+    unsafe {
+        tigr::tigrRGB(
+            r as libc::c_int as libc::c_uchar,
+            g as libc::c_int as libc::c_uchar,
+            b as libc::c_int as libc::c_uchar
+        )
+    }
+}
+
